d792d46bd496b9ca8997ffc0adefc4b868a6b617
Nourelden Rihan <noureldenrihan@gmail.com>
2022-03-18 13:17:04 +0200

    Added Script to Get Current Pizza Count

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch("http://pizza-data-api.herokuapp.com/pizzas")
+  .then((res) => res.json())
+  .then((data) => renderPizzaNum(data.length));
+
+function renderPizzaNum(num) {
+  let pizzaNumSpan = document.getElementById("num");
+  pizzaNumSpan.innerHTML = num;
+}
